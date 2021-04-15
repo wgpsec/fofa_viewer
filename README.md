01680b72b347dd00e2ace57e97e9e0452ba65fe5
keac <admin@wgpsec.org>
2021-04-15 10:18:47 +0800

    :recycle: 重构 IconHash 实现JAVA计算方式

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-<h1 align="center">fofa_viewer </h1>
+<h1 align="center">Fofa_Viewer 🔗 </h1>
 <p align="center"> 
 <img src="https://img.shields.io/badge/JDK-1.8-green">
-<img src="https://img.shields.io/badge/version-1.0.1-brightgreen">
+<img src="https://img.shields.io/badge/version-1.0.2-brightgreen">
 <img src="https://img.shields.io/badge/author-f1ashine-orange">
 <img src="https://img.shields.io/badge/WgpSec-%E7%8B%BC%E7%BB%84%E5%AE%89%E5%85%A8%E5%9B%A2%E9%98%9F-blue">
 </p>
+
 
 
 
@@ -12,6 +13,7 @@
 ##  简介
 一个简单易用的fofa客户端  
 使用javafx编写，便于跨平台使用
+
 ## :sparkles: 功能
 1. 多标签式查询结果展示
 2. 丰富的右键菜单
@@ -20,7 +22,7 @@
 
 <img src="https://f1ashine.gitee.io/research_pic/fofa_viewer/contextMenu.png">
 
-## :rocket: 手动编译
+## :rocket: 二次开发
 ![](https://f1ashine.gitee.io/research_pic/fofa_viewer/compile_detail.png)
 
 idea打开项目，等待依赖包下载完毕后直接双击Plugins-assembly-assembly:assembly，然后将target文件夹中带有"with-dependencies"的jar包拷贝到带有config.propertiese的文件夹再运行即可。
@@ -28,9 +30,7 @@ idea打开项目，等待依赖包下载完毕后直接双击Plugins-assembly-as
 ![](https://f1ashine.gitee.io/research_pic/fofa_viewer/maven_detail.png)
 
 ## ⚠️ 说明
-- 使用前需要在`config.properties`中配置`email`和`key`才能正常使用  
-
-- 项目使用了[iconhash](https://github.com/Becivells/iconhash) 来计算icon hash，其中windows的exe版本可能会被杀软误报，建议加白，当然也可以去源地址重新下载。  
+- 使用前需要在`config.properties`中配置`email`和`key`才能正常使用
 
 - 项目中配置了error.log，如果有需要提bug，希望能带上这个截图，另外也欢迎提issue帮助改进。
 
--- src/main/java/org/fofaviewer/utils/RequestHelper.java
+++ src/main/java/org/fofaviewer/utils/RequestHelper.java
@@ -13,6 +13,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import sun.misc.BASE64Encoder;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.URL;
@@ -20,19 +21,23 @@ import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.google.common.hash.Hashing;
 
 public class RequestHelper {
     private static RequestHelper request = null;
     private Logger logger = null;
     private static final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
     private final String[] ua = new String[]{
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.41 Safari/537.36 Edg/88.0.705.22"
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.41 Safari/537.36 Edg/88.0.705.22"
     };
     private final String path = System.getProperty("user.dir") +
             System.getProperty("file.separator") + "iconhash" + System.getProperty("file.separator");
@@ -42,31 +47,31 @@ public class RequestHelper {
         LogUtil.setLogingProperties(logger);
     }
 
-    public static RequestHelper getInstance(){
-        if(request==null){
+    public static RequestHelper getInstance() {
+        if (request == null) {
             request = new RequestHelper();
         }
         return request;
     }
 
-    private CloseableHttpResponse getResponse(String url){
+    private CloseableHttpResponse getResponse(String url) {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         CloseableHttpResponse response = null;
-        HashMap<String,String> map = new HashMap<String,String>();
+        HashMap<String, String> map = new HashMap<String, String>();
         try {
             URIBuilder builder = new URIBuilder(url);
             HttpGet httpGet = new HttpGet(builder.build());
             httpGet.setHeader("User-Agent", ua[(new SecureRandom()).nextInt(3)]);
             response = httpClient.execute(httpGet);
             return response;
-        }catch (java.net.ConnectException e){
+        } catch (java.net.ConnectException e) {
             logger.log(Level.WARNING, e.getMessage(), e);
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
             alert.setContentText("网站访问异常！");
             alert.showAndWait();
             return null;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.log(Level.WARNING, ex.getMessage(), ex);
             return null;
         }
@@ -74,39 +79,40 @@ public class RequestHelper {
 
     /**
      * 发起HTTP请求获取响应内容
+     *
      * @param url 请求url
      * @return 响应内容
      * 200 : 请求响应内容
      * other code : request error
      * error ：请求失败
      */
-    public HashMap<String, String> getHTML(String url){
+    public HashMap<String, String> getHTML(String url) {
         CloseableHttpResponse response = getResponse(url);
-        HashMap<String,String> result = new HashMap<>();
-        if(response != null){
+        HashMap<String, String> result = new HashMap<>();
+        if (response != null) {
             int code = response.getStatusLine().getStatusCode();
             result.put("code", String.valueOf(code));
-            try{
-                if(code == 200){
+            try {
+                if (code == 200) {
                     HttpEntity httpEntity = response.getEntity();
-                    result.put("msg", EntityUtils.toString(httpEntity,"utf8"));
-                }else if(code == 401) {
+                    result.put("msg", EntityUtils.toString(httpEntity, "utf8"));
+                } else if (code == 401) {
                     result.put("msg", "请求错误状态码401，可能是没有在config中配置有效的email和key，或者您的账号权限不足无法使用api进行查询。");
-                }else if(code == 502){
+                } else if (code == 502) {
                     result.put("msg", "请求错误状态码502，可能是账号限制了每次请求的最大数量，建议尝试修改config中的maxSize为100");
-                }else{
+                } else {
                     result.put("msg", "请求响应错误,状态码" + String.valueOf(code));
                 }
                 return result;
-            }catch (Exception e){
+            } catch (Exception e) {
                 result.put("code", "error");
                 result.put("msg", e.getMessage());
                 logger.log(Level.WARNING, e.getMessage(), e);
                 return result;
-            }finally {
-                try{
+            } finally {
+                try {
                     response.close();
-                }catch (IOException ex) {
+                } catch (IOException ex) {
                     logger.log(Level.WARNING, ex.getMessage(), ex);
                 }
             }
@@ -119,21 +125,22 @@ public class RequestHelper {
      * 提取网站favicon 需要两步：
      * 1. 直接访问url根目录的favicon，若404则跳转至第2步
      * 2. 访问网站，获取html页面，获取head头中的 link标签的ico 路径
+     *
      * @param url
      * @return
      */
-    public HashMap<String, String> getImageFavicon(String url){
-        String filename =  this.path + "favicon.ico";
+    public HashMap<String, String> getImageFavicon(String url) {
+        String filename = this.path + "favicon.ico";
         File file = new File(filename);
         CloseableHttpResponse response = getResponse(url);
-        HashMap<String,String> result = new HashMap<>();
+        HashMap<String, String> result = new HashMap<>();
         int cache = 10240;
-        if(response !=null) {
+        if (response != null) {
             int code = response.getStatusLine().getStatusCode();
             result.put("code", String.valueOf(code));
             if (code == 200) {
                 try {
-                    if(response.getEntity().getContentLength() == 0){
+                    if (response.getEntity().getContentLength() == 0) {
                         logger.log(Level.FINE, url + "无响应内容");
                         return null;
                     }
@@ -147,20 +154,20 @@ public class RequestHelper {
                     is.close();
                     fileout.flush();
                     fileout.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                     result.put("code", "error");
                     result.put("msg", e.getMessage());
                     logger.log(Level.WARNING, e.getMessage(), e);
                     return result;
-                }finally{
-                    try{
+                } finally {
+                    try {
                         response.close();
-                    }catch (IOException ex) {
+                    } catch (IOException ex) {
                         logger.log(Level.WARNING, ex.getMessage(), ex);
                     }
                 }
                 String hash = getIconHash(filename);
-                if(hash != null) {
+                if (hash != null) {
                     result.put("msg", hash.replaceAll("\n", ""));
                     return result;
                 }
@@ -169,21 +176,21 @@ public class RequestHelper {
         return null;
     }
 
-    public String getLinkIcon(String url){
+    public String getLinkIcon(String url) {
         HashMap<String, String> result = getHTML(url);
-        if(result.get("code").equals("200")){
+        if (result.get("code").equals("200")) {
             Document document = Jsoup.parse(result.get("msg"));
             Elements elements = document.getElementsByTag("link");
-            if (elements.size() == 0){ // 没有link标签
+            if (elements.size() == 0) { // 没有link标签
                 return null;
-            }else{
-                for(Element i : elements){
+            } else {
+                for (Element i : elements) {
                     String rel = i.attr("rel");
-                    if(rel.equals("icon") || rel.equals("shortcut icon")){
+                    if (rel.equals("icon") || rel.equals("shortcut icon")) {
                         String href = i.attr("href");
-                        if(href.startsWith("http")){ // link 显示完整url
+                        if (href.startsWith("http")) { // link 显示完整url
                             return href;
-                        }else if(href.startsWith("/")){  // link 显示相对路径
+                        } else if (href.startsWith("/")) {  // link 显示相对路径
                             return url + href;
                         }
                         return null;
@@ -193,58 +200,21 @@ public class RequestHelper {
         }
         return null;
     }
+
     /**
      * 使用iconhash计算favicon hash
+     *
      * @param f favicon的文件对象
      * @return favicon hash值
      */
-    private String getIconHash(String f){
+    private String getIconHash(String f) {
         String os = System.getProperty("os.name").toLowerCase(Locale.US);
         String arch = System.getProperty("os.arch").toLowerCase(Locale.US);
         StringBuilder filepath = new StringBuilder();
-        if(arch.equals("amd64") || arch.equals("x86_64")){
-            if(os.contains("mac")){
-                filepath.append(this.path).append("iconhash_darwin_amd64");
-            }else if(os.contains("windows")){
-                filepath.append(this.path).append("iconhash_windows_amd64.exe");
-            }else {//linux
-                filepath.append(this.path).append("iconhash_linux_amd64");
-            }
-            Process process = null;
-            BufferedReader bufrIn = null;
-            BufferedReader bufrError = null;
-            StringBuilder result = new StringBuilder();
-            try {
-                process = Runtime.getRuntime().exec(new String[]{filepath.toString(), "-file", f});
-                process.waitFor();
-                bufrIn = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
-                bufrError = new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
-                String line;
-
-                while ((line = bufrIn.readLine()) != null) {
-                    result.append(line).append('\n');
-                }
-                while ((line = bufrError.readLine()) != null) {
-                    result.append(line).append('\n');
-                }
-            }catch (Exception e){
-                logger.log(Level.WARNING, e.getMessage(), e);
-            }finally {
-                try {
-                    if (bufrIn != null) bufrIn.close();
-                    if (bufrError != null) bufrIn.close();
-                }catch (Exception ex){
-                    logger.log(Level.FINER, ex.getMessage(), ex);
-                }
-                // 销毁子进程
-                if (process != null) {
-                    process.destroy();
-                }
-            }
-            return result.toString();
-        }else{
-            return null;
-        }
+        System.out.println(f);
+        String base64Str = ImageToBase64(f);
+        int murmu = Hashing.murmur3_32().hashString(base64Str.replaceAll("\r", "") + "\n", StandardCharsets.UTF_8).asInt();
+        return String.valueOf(murmu);
     }
 
     public String getCertSerialNum(String host) {
@@ -256,18 +226,36 @@ public class RequestHelper {
             Certificate[] certs = conn.getServerCertificates();
             X509Certificate cert = (X509Certificate) certs[0];
             return "cert=\"" + cert.getSerialNumber().toString() + "\"";
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.log(Level.FINER, e.getMessage(), e);
         }
         return null;
     }
 
+    private static String ImageToBase64(String imgPath) {
+        byte[] data = null;
+        // 读取图片字节数组
+        try {
+            InputStream in = new FileInputStream(imgPath);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        // 返回Base64编码过的字节数组字符串
+        return encoder.encode(Objects.requireNonNull(data));
+    }
+
     /**
      * base64编码字符串
+     *
      * @param str 字符串
      * @return 编码字符串
      */
-    public static String encode(String str){
+    public static String encode(String str) {
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(str.getBytes(StandardCharsets.UTF_8));
     }
